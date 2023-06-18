2aaf3224f88c96e3ea332e1729f2d63574ae21e7
resze <ge26feb@tum.de>
2023-06-18 18:43:09 +0200

    updated server & client for Pi

--- server.py
+++ server.py
@@ -4,20 +4,20 @@ import random
 from time import sleep
 
 server = ModbusServer('192.168.178.31',port = 502, no_block = True)
-#try:
-print("Start server...")
-server.start()
-print("Server is online")
-state = [0]
-while True:
-    state = server.data_bank.get_holding_registers(0)
-    if state != server.data_bank.get_holding_registers(1):
-        state = server.data_bank.get_holding_registers(1)
-        print('New value is equal to:', str(state))
-    else:
-        server.data_bank.set_holding_registers(0, state)
+try:
+    print("Start server...")
+    server.start()
+    print("Server is online")
+    state = [0]
+    while True:
+        state = server.data_bank.get_holding_registers(0)
+        if state != server.data_bank.get_holding_registers(1):
+            state = server.data_bank.get_holding_registers(1)
+            print('New value is equal to:', str(state))
+        else:
+            server.data_bank.set_holding_registers(0, state)
     sleep(1)
-#except:
- #   print('Shutdown server...')
-  #  server.stop()
-   # print("Server is offline")
+except:
+    print('Shutdown server...')
+    server.stop()
+    print("Server is offline")
