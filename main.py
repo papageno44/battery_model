a0c4a246a9c872e2d06f244bb315e077a566bb4c
resze <ge26feb@tum.de>
2023-07-01 21:02:15 +0200

    pi_model.py runs on linux subsystem, model.py on windows

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyModbusTCP.client import ModbusClient
 import time
-import keyboard
+import pandas as pd
 import threading
 
 
@@ -20,62 +20,105 @@ def connect_to_battery(server_ip='192.168.178.102', port=12345):
     return client
 
 
-def start_simulation(client, initial_soc=0.5):
+def start_simulation(client, initial_soc=0.5, dt=1):
     switch = False
 
     while not switch:
         input('Press any key to start a simulation')
         initial_soc = input('Input initial SoC (0 to 1)')
+        initial_soc = int(float(initial_soc) * 100)
+
+        dt = int(input('Input time step in [s]'))
         client.write_single_coil(0, True)  ## On/Off coil
-        global run_sim = True
-        client.write_single_register(3, initial_soc)
+        global RUN_SIM
+        RUN_SIM = True
+        client.write_single_register(4, initial_soc)
+        client.write_single_register(5, dt)
         switch = 1
     return print('Successfully started the simulation')
 
 
-def keyboard_input_start():
-    input_thread = threading.Thread(target=check_input())
-    input_thread.daemon = True
-    input_thread.start()
+# def keyboard_input_start():
+#     input_thread = threading.Thread(target=check_input())
+#     input_thread.daemon = True
+#     input_thread.start()
 
 
-def check_input():
-    while pressed_key == None:
-        pressed_key = input('Type a number to change input current. Type [q] to stop the simulation')
-        if pressed_key.isdigit():
-            pressed_key = float(pressed_key)
+class KeyboardThread(threading.Thread):
+
+    def __init__(self, input_cbk=None, name='keyboard-input-thread'):
+        self.input_cbk = input_cbk
+        super(KeyboardThread, self).__init__(name=name)
+        self.start()
+
+    def run(self):
+        while True:
+            self.input_cbk(input(
+                'Type a number to change input current in [A]. Type [q] to stop the simulation'))  # waits to get input + Return
+
+
+def my_callback(inp):
+    global PRESSED_KEY
+    PRESSED_KEY = inp
+    if PRESSED_KEY.isdigit():
+        PRESSED_KEY = float(PRESSED_KEY)
+        print('pressed key is a digit!')
+    print('You Entered:', inp, )
+
+
+# def check_input():
+#     global PRESSED_KEY
+#     while PRESSED_KEY is None:
+#         PRESSED_KEY = input('Type a number to change input current in [A]. Type [q] to stop the simulation')
+#         if PRESSED_KEY.isdigit():
+#             PRESSED_KEY = float(PRESSED_KEY)
 
 
 def forward_input_to_battery():
-    if pressed_key is not None:
-        if pressed_key == 'q':
-            client.write_single_coil(1, False)  ## Turn off simulation
-            pressed_key = None
-        if isinstance(pressed_key, float):
-            client.write_single_register(0, pressed_key)
-            pressed_key = None
+    global PRESSED_KEY
+    if PRESSED_KEY is not None:
+        if PRESSED_KEY == 'q':
+            client.write_single_coil(0, False)  ## Turn off simulation
+            PRESSED_KEY = None
+        if isinstance(PRESSED_KEY, float):
+            input_current = int(round(PRESSED_KEY * 100, 2))
+            print('current changed to', PRESSED_KEY)
+            client.write_single_register(0, input_current)
+            PRESSED_KEY = None
 
 
 def read_variables(client):
-    current = client.read_holding_registers(0)[0]
-    voltage = client.read_holding_registers(1)[0]
-    soc = client.read_holding_registers(2)[0]
-    global run_sim
-    run_sim = client.read_coils(1)[0]
+    current = client.read_holding_registers(0)[0] / 100
+    voltage = client.read_holding_registers(1)[0] / 10
+    soc = client.read_holding_registers(2)[0] / 100
+    time = client.read_holding_registers(3)[0]
+    print('current: ', current, 'voltage: ', voltage, 'soc: ', soc, 'time: ', time)
+    soc_0 = client.read_holding_registers(4)[0] / 100
+    time_step = client.read_holding_registers(5)[0]
+    global RUN_SIM, CURRENT_LIST, VOLTAGE_LIST, SOC_LIST, TIME_LIST
+    RUN_SIM = client.read_coils(0)[0]
     CURRENT_LIST.append(current)
     VOLTAGE_LIST.append(voltage)
     SOC_LIST.append(soc)
+    TIME_LIST.append(time)
 
 
-client = connect_to_battery()
+client = connect_to_battery(server_ip='172.25.111.167')
 start_simulation(client)
-run_sim = 1
+RUN_SIM = 1
 CURRENT_LIST = []
 VOLTAGE_LIST = []
 SOC_LIST = []
-keyboard_input_start()
-
-while run_sim:
+TIME_LIST = []
+PRESSED_KEY = None
+# keyboard_input_start()
+# start the Keyboard thread
+kthread = KeyboardThread(my_callback)
+while RUN_SIM:
     forward_input_to_battery()
     read_variables(client)
     time.sleep(1)
+
+output_df = pd.DataFrame(data=dict(Time=TIME_LIST, Current=CURRENT_LIST, Voltage=VOLTAGE_LIST, SoC=SOC_LIST))
+print('time list; : ', TIME_LIST)
+print('output_df', output_df)
--- pi_model.py
+++ pi_model.py
@@ -6,7 +6,7 @@ from pyModbusTCP.server import ModbusServer
 # Turn on the battery
 def turn_on_battery(server='192.168.178.102', port=12345):
     server = ModbusServer(server, port=port, no_block=True)
-    print("Start server...")
+    print("Battery is on")
     server.start()
     return server
 
@@ -16,7 +16,8 @@ def turn_on_battery(server='192.168.178.102', port=12345):
 def initialize_simulation(server):
     model = pybamm.lithium_ion.SPMe()
     param = pybamm.ParameterValues("Chen2020")
-    initial_soc = server.data_bank.get_holding_registers(3)[0]
+    initial_soc = float(server.data_bank.get_holding_registers(4)[0]) / 100
+    print('initial soc= ', initial_soc)
     param["Current function [A]"] = "[input]"
     sim = pybamm.Simulation(model, parameter_values=param)
     sim.build(check_model=True, initial_soc=initial_soc)
@@ -29,31 +30,44 @@ def rescale_soc(calculated_soc, min_soc, max_soc):
     return soc
 
 
+def reverse_rescale_soc(wanted_soc, min_soc, max_soc):
+    calculated_soc = wanted_soc*(max_soc - min_soc) + min_soc
+    return calculated_soc
+
+
 def time_step(server, current, sim, param):
-    initial_soc = server.data_bank.get_holding_registers(3)[0]
+    initial_soc = float(server.data_bank.get_holding_registers(4)[0])
+    initial_soc = reverse_rescale_soc(initial_soc, -0.023, 0.933) / 100
+    print('initial_soc', initial_soc)
+    time_step = server.data_bank.get_holding_registers(5)[0]
     sim.step(dt=time_step, npts=2, save=True, inputs={'Current function [A]': current})
     solution = sim.solution
     n = len(solution["Time [s]"].entries) - 1
     time = solution["Time [s]"].entries[n]
     discharge_capacity = solution['Discharge capacity [A.h]'].entries[n]
-    soc = rescale_soc(initial_soc - discharge_capacity / param['Nominal cell capacity [A.h]'])
-    voltage = round(solution['Voltage [V]'].entries[n], 2)
-    write_current_variables(server, soc, voltage)
+    soc = initial_soc - discharge_capacity / param['Nominal cell capacity [A.h]']
+    soc = rescale_soc(soc, -0.023, 0.933) * 100
+    soc = int(round(soc, 1))
+    voltage = int(round(solution['Voltage [V]'].entries[n], 1) * 10)
+    write_current_variables(server, soc, voltage, time)
 
 
-def write_current_variables(server, soc, voltage):
-    server.data_bank.set_holding_registers(1, voltage)
-    server.data_bank.set_holding_registers(2, soc)
+def write_current_variables(server, soc, voltage, time):
+    server.data_bank.set_holding_registers(1, [voltage])
+    server.data_bank.set_holding_registers(2, [soc])
+    server.data_bank.set_holding_registers(3, [time])
 
 
-server = turn_on_battery()
+server = turn_on_battery(server='172.25.111.167')
 switch = False
 while not switch:
     switch = server.data_bank.get_coils(0)[0]
-sim = initialize_simulation(server)[0]
-param = initialize_simulation(server)[1]
+init = initialize_simulation(server)
+sim = init[0]
+param = init[1]
 while switch:
-    current = server.data_bank.get_holding_registers(0)[0]
+    current = server.data_bank.get_holding_registers(0)[0] / 100
+    print('current:', current)
     switch = server.data_bank.get_coils(0)[0]
     time_step(server, current, sim, param)
     time.sleep(1)
