c6217b14b0784471cd3da877060a1a856aab3eab
resze <ge26feb@tum.de>
2023-07-24 15:49:55 +0200

    timing continuity improvement

--- pi_model.py
+++ pi_model.py
@@ -20,7 +20,7 @@ def initialize_simulation(server):
     initial_soc = float(server.data_bank.get_holding_registers(4)[0]) / 100
     param["Current function [A]"] = "[input]"
     param["Electrode width [m]"] = float(server.data_bank.get_holding_registers(6)[0]) / 100
-    param["Nominal cell capacity [A.h]"] = 5*param["Electrode width [m]"]/1.58
+    param["Nominal cell capacity [A.h]"] = 5 * param["Electrode width [m]"] / 1.58
     sim = pybamm.Simulation(model, parameter_values=param)
     sim.build(check_model=True, initial_soc=initial_soc)
     print('Successfully initialized the simulation!')
@@ -37,7 +37,7 @@ def reverse_rescale_soc(wanted_soc, min_soc, max_soc):
     return calculated_soc
 
 
-def time_step(server, current, sim, param):
+def time_step(server, current, sim, param, old_time):
     initial_soc = float(server.data_bank.get_holding_registers(4)[0]) / 100
     initial_soc = reverse_rescale_soc(initial_soc, -0.02332, 0.9773)
     time_step = server.data_bank.get_holding_registers(5)[0]
@@ -49,23 +49,29 @@ def time_step(server, current, sim, param):
     solution = sim.solution
     n = len(solution["Time [s]"].entries) - 1
     time = solution["Time [s]"].entries[n]
-    discharge_capacity = solution['Discharge capacity [A.h]'].entries[n]
-    print('Discharge capacity:', discharge_capacity)
-    soc = initial_soc - discharge_capacity / param['Nominal cell capacity [A.h]']
-    soc = rescale_soc(soc, -0.02332, 0.9773)
-    soc = int(round(soc * 100, 0))
-    voltage = int(round(solution['Voltage [V]'].entries[n], 1) * 10)
-    if voltage >= (4.2 * 10)*0.95:
-        print('Max voltage voltage was reached!')
-        server.data_bank.set_coils(2, [True])
-    else:
-        server.data_bank.set_coils(2, [False])
-    if voltage <= (2.5 * 10)*1.05:
-        print('Min voltage voltage was reached!')
-        server.data_bank.set_coils(3, [True])
-    else:
-        server.data_bank.set_coils(3, [False])
-    write_current_variables(server, soc, voltage, time)
+    if time != old_time:
+        if time != old_time + time_step:
+            n = n - 2
+            time = solution["Time [s]"].entries[n]
+        discharge_capacity = solution['Discharge capacity [A.h]'].entries[n]
+        print('Discharge capacity:', discharge_capacity)
+        soc = initial_soc - discharge_capacity / param['Nominal cell capacity [A.h]']
+        soc = rescale_soc(soc, -0.02332, 0.9773)
+        soc = int(round(soc * 100, 0))
+        voltage = int(round(solution['Voltage [V]'].entries[n], 1) * 10)
+        if voltage >= (4.2 * 10) * 0.95:
+            print('Max voltage voltage was reached!')
+            server.data_bank.set_coils(2, [True])
+        else:
+            server.data_bank.set_coils(2, [False])
+        if voltage <= (2.5 * 10) * 1.05:
+            print('Min voltage voltage was reached!')
+            server.data_bank.set_coils(3, [True])
+        else:
+            server.data_bank.set_coils(3, [False])
+        write_current_variables(server, soc, voltage, time)
+    old_time = time
+    return old_time
 
 
 def write_current_variables(server, soc, voltage, time):
@@ -77,6 +83,7 @@ def write_current_variables(server, soc, voltage, time):
 server = turn_on_battery(server='192.168.178.105')
 switch = False
 repeat = True
+old_time = 0
 while repeat:
     while not switch:
         switch = server.data_bank.get_coils(0)[0]
@@ -90,6 +97,6 @@ while repeat:
             current = -current
         print('Current:', current)
         switch = server.data_bank.get_coils(0)[0]
-        time_step(server, current, sim, param)
+        old_time = time_step(server, current, sim, param, old_time)
         time.sleep(1)
     print('The simulation was stopped.')
